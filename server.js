ebd3145ce653a1bb73e58475381dc251e37daf6b
walterguo <taguo92@gmail.com>
2017-05-07 18:50:33 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -5,46 +5,50 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-//app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-     var now = new Date().toString();
-     var log = `${now}: ${req.method} ${req.url}`;
-     console.log(log);
-     fs.appendFile('server.log', log + '\n');
-     next();
-})
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear',() => {
- return new Date().getFullYear();
-})
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
-hbs.registerHelper('screamIt',(text) => {
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
-})
+});
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear()
-  })
-})
-
-app.get('/about',(req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
-app.get('/bad', (req,res) => {
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
-  })
+  });
 });
 
 app.listen(port, () => {
